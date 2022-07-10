e1eb409414d8f7666617be6d34bbba2af8d5ee35
AA-Stargazer <mforstrtp@protonmail.com>
2022-07-10 07:45:55 +0000

    set grid size input functionality

--- javascript.js
+++ javascript.js
@@ -15,14 +15,19 @@ let mouseDown = false;
 document.onmousedown = () => { mouseDown = true }
 document.onmouseup = () => { mouseDown = false }
 
-let colorInput = document.querySelector('#colorInput');
-
+let colorInput = document.querySelector('#color-input');
+let gridSizeInput = document.querySelector('#grid-size-input');
 
 
 // TODO row number not updated along with gridSize!!!
 
 colorInput.addEventListener('change', updateColor);
+
+gridSizeInput.value = gridSize;
+gridSizeInput.addEventListener('change', updateGridArea);
+
 let currentColor = colorInput.value;
+
 
 
 // ---------------------------------------------------------------------------------------------
@@ -44,6 +49,15 @@ function containerCreate() {
 			tmpPixel.style.height = `100%`;
 			tmpPixel.style.width = `${pixelSize}px`;
 
+
+			// TODO if you can, or could be done... 
+			// https://www.geeksforgeeks.org/event-delegation-in-javascript/
+			// https://javascript.info/event-delegation
+			// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Events#event_delegation
+			// I was thinking something like,  take the relative mouse location etc and affect to the particular element which the mouse on.... but event-delegation looks pretty good and better than what I could do with that relative-mouse location (also could be slow too...)
+
+
+
 			// this works while sliding the cursor through the container...
 			tmpPixel.addEventListener('mouseenter',
 				() => {
@@ -56,12 +70,11 @@ function containerCreate() {
 			);
 
 			// this works for single clicks
-			tmpPixel.addEventListener('click',
+			tmpPixel.addEventListener('mousedown',
 				() => {
 					tmpPixel.style.backgroundColor = currentColor;
 				}
 			);
-
 
 			tmpArray.push(tmpPixel);
 		}
@@ -95,6 +108,8 @@ function containerClean() {
 
 function createDrawArea() {
 	pixelSize = getPixelSize();
+
+	containerClean();
 	containerCreate();
 	console.log();
 	containerPlaceDivs();
@@ -105,10 +120,13 @@ function updateColor() {
 	currentColor = colorInput.value;
 }
 
+function updateGridArea() {
+	gridSize = gridSizeInput.value;
+	createDrawArea();
+}
 
 
-
-
+createDrawArea();
 
 
 
