db582596d4d70e89428f8b6d581edc9d3d3ecbe1
AA-Stargazer <mforstrtp@protonmail.com>
2022-07-12 15:35:32 +0000

    try first ajax with local file, CORS error

--- javascript.js
+++ javascript.js
@@ -46,20 +46,38 @@ let currentColor = colorInput.value;
 // TODO add favicon option to set everything egiible to create a favicon...
 
 // UPDATE: might need to use alpha value between 0-255 instead of 0-1, for python PIL
-// NOTE: I waas gonna use C or so, but I couldn't find a solution like I want, so I turned to the python....
-// but I encountered some cool sites like: https://www.usna.edu/Users/cs/nchamber/courses/si204/s18/lab/l08/lab.html
-// NOTE: for using databases with javascript https://www.quora.com/Should-I-use-JavaScript-to-query-MySQL-database-on-my-website-or-should-I-use-PHP-Python-Ruby
-// -- // from Estevan Seneca:
-// -- // If you are referring to browser based Javascript, you do not want to have it pass SQL statements directly to the server. This presents a major liability since you’re exposing your data store to a host of input validation risks. Any client side user would be able to modify the in browser request and feed it directly to your server.
+
+// as I saw here while to search for running python in js https://stackoverflow.com/a/13175665
+// Now I'm learning something called AJAX (finally, I can scrape the websites better I guess :D)
+// https://www.w3schools.com/js/js_ajax_intro.asp
+// -- // AJAX is a developer's dream, because you can:
 // -- // 
-// -- // If you’re referring to your stack, go with what is already available. If your existing infrastructure uses Python/Django then SQLAlchemy or a similar ORM my work for you. PHP has an excellent set of database bindings for a majority of databases as well.
-// -- // 
-// -- // If you’re starting from scratch then I’d suggest what ever would integrate into your ecosystem the best. Look at what you know, what you (or your company) already runs and make your decision that would allow for the easiest integration and testing.
-// -- // 
-// -- // If we knew a little bit more about the circumstance in which you are operating in, a better advisement could be given.
-// so using database etc is risky with javascript like using innerHTML from the user-input... now ruby etc makes sense to be honest.... actually I can do something like, if the sql code is equal to what we want... But then again, we would have to be doing this with another .js file... Anyway, I didn't need in this stage for such a thing, I guess (I hope still won't have risk, but will use the files... -_-), But I thought wouldn't be bad to use some sql, db etc...
-// another opinion on using sql with javascript: https://stackoverflow.com/a/49237650
-// Still though, I'll firstly complete the javascript back-end path first. I don't need some database thing for a while. And can start to do some things/applications with node.js
+// -- //     Read data from a web server - after the page has loaded
+// -- //     Update a web page without reloading the page
+// -- //     Send data to a web server - in the background
+
+// I wasn't sure but if it'll be there until late half 21st century, then it would worth it https://www.reddit.com/r/ruby/comments/lywt2z/is_ruby_rails_still_worth_learning_in_2021_and_is/
+// python has too many coder (users are spreadout, not depends on the programming language but popularity, relaibility etc...), the risk almost same for me... Just require me (as I have already should do it like) to believe and keep working...
+
+
+// Ajax links I'm going through in order (up to some point)
+// https://www.w3schools.com/js/js_ajax_intro.asp
+// https://www.w3schools.com/js/js_ajax_http.asp
+// - // The keystone of AJAX is the XMLHttpRequest object.
+// - //     Create an XMLHttpRequest object
+// - //     Define a callback function
+// - //     Open the XMLHttpRequest object
+// - //     Send a Request to a server
+// -- // For security reasons, modern browsers do not allow access across domains.
+// -- // XMLHttpRequest Object Methods
+// -- // XMLHttpRequest Object Properties 
+// -- // ...
+// https://www.rubyguides.com/2019/03/rails-ajax/ (this is maybe later to me...)
+// https://developer.mozilla.org/en-US/docs/Web/Guide/AJAX/Getting_Started (finally, I can understand the onready, .open, .ready )
+// -- // hmm, this readState, we can do loading pages etc with if statements and global values (for asynchronous, you know, like keepiing the images for example in rock-paper-scissors)  inside the file..
+
+
+
 
 
 
@@ -213,5 +231,72 @@ function hexToRgb(hex_color) {
 function floatTo255(number) {
 	return (number * 255).toFixed();
 }
+
+
+
+
+
+// ---------------------------------------------------------------------------------------------
+//
+// ------------------------------------------- AJAX --------------------------------------------
+
+let ajaxButton = document.querySelector('#ajax-try');
+
+ajaxButton.addEventListener('click', ajaxy);
+
+// 2022-07-12
+// when I clicked to the button, error in the console: "" Cross-Origin Request Blocked: The Same Origin Policy disallows reading the remote resource at file:///home/arc_dust/theodinproject/etAsketch/array_to_image.py. (Reason: CORS request not http). (Learn More). ""
+// In the '(Learn More)'   (https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS/Errors/CORSRequestNotHttp), It directly got section for local file    https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS/Errors/CORSRequestNotHttp:
+// -- // Loading a local file
+// -- // 
+// -- // Local files from the same directory and subdirectories were historically treated as being from the same origin. This meant that a file and all its resources could be loaded from a local directory or subdirectory during testing, without triggering a CORS error.
+// -- // 
+// -- // Unfortunately this had security implications, as noted in this advisory: CVE-2019-11730. Many browsers, including Firefox and Chrome, now treat all local files as having opaque origins (by default). As a result, loading a local file with included local resources will now result in CORS errors.
+// -- // 
+// -- // Developers who need to perform local testing should now set up a local server. As all files are served from the same scheme and domain (localhost) they all have the same origin, and do not trigger cross-origin errors.
+// -- // 
+// -- // Note: This change is in line with the URL specification, which leaves the origin behavior for files to the implementation, but recommends that file origins are treated as opaque if in doubt.
+
+
+
+
+
+
+function ajaxy() {
+	if (window.XMLHttpRequest)
+		httpRequest = new XMLHttpRequest();
+	else if (window.ActiveXObject)
+		httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+	
+	httpRequest.onreadstatechange = function() {
+	}
+
+	httpRequest.open('GET', './array_to_image.py', true);
+	httpRequest.send()
+
+
+	if (httpRequest.readyState == XMLHttpRequest.DONE) {
+	
+		console.log(httpRequest.readyState)
+		if (httpRequest.status == 200) {
+			// perfect
+			console.log(httpRequest.responseText);
+		}
+		else {
+			// whoopsie, there was a problem...
+		}
+		
+	}
+	else {
+		// Not yet
+	}
+
+}
+
+
+
+
+
+
 
 
